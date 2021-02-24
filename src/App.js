f6562482360d0f53093763fe20b54678446c69e1
Samayun Chowdhury <samayunmc99@gmail.com>
2021-02-24 14:27:09 +0600

    learnt useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/card.css';
+import React,{useState,useEffect} from 'react'
+
+
+const styleHeader = {
+  backgroundColor: '#282c34',
+  minHeight: "10vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "calc(10px + 2vmin)",
+  color: "white"
+}
+const container = { width: "90%", margin: "auto" }
+const card = {
+  listStyle: "none",
+  padding: "10px 15px",
+  background: "#f0f0f0",
+  margin: "1px",
+  columnCount: "2"
+}
+
 
 function App() {
+  const [appName,setAppName] = useState("My React Application");
+  const [students,setStudents] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(res => res.json())
+    .then(students => {
+      setIsLoading(false);
+      setStudents(students);
+    })
+    .catch(err => {
+      setIsLoading(false);
+      setAppName(err.message);
+    })
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+   <React.Fragment>
+     { isLoading ? <Loading />  : <div >
+      <header style={styleHeader}>
+        {appName} 
+        <span> <button className="btn btn-success" onClick={ () => setAppName("React Application") }> Change Name </button> </span>
       </header>
-    </div>
+     { students.length &&  <section style={{display:'grid',...container}}  >
+        {students.map(st => <Student student={st} key={st.id} />)}
+      </section> }
+    </div>}
+   </React.Fragment>
   );
 }
 
+function Student(props) {
+  let {name,phone,age,username,email,website} = props.student;
+  return <div className="card" style={card}>
+            <div className="left">
+                <h2>{name} <sup style={{color:'#000',fontSize:'0.8rem'}}> [{username}] </sup></h2>
+                {email && <h4> Email :  {email} </h4>}
+            </div>
+            <div className="right">
+              { phone && <p>Phone : {phone}</p>}
+              { website && <p>website : <a target="_blank" rel="noreferrer"  href={`http://${website}`}>{website}</a> </p>}
+              { age && <p>Age : {age}</p>}
+            </div>
+        </div>
+      }
+
+function Loading(){
+  return <div className="loading" style={container}> <h2>Loading.......</h2></div>
+}
+
 export default App;
+
---
+++ src/components/Student.js
@@ -0,0 +1,4 @@
+const students = [{id:1,name:"Akash Rahman",age:21},{id:1,name:"Alp Arsalan",age:17},{id:1,name:"Tanisha Rahman",age:20},{id:1,name:"Malik Shah",age:15}]
+
+
+console.log(JSON.stringify(students));
